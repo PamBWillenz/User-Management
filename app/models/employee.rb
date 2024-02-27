7c107303dd76698cebd52d5af1b0bcd5c36f0565
PamBWillenz <pam@positek.net>
2024-02-27 14:34:40 -0500

    add find age method in model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,12 +3,11 @@ class Employee < ApplicationRecord
     validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
     validates :contact_number, format: { with: /\d{10}/, message: "must be a 10-digit phone number" }
 
-    # Additional validations for date_of_birth and date_of_hiring
-    # validates :date_of_birth, custom_validation_method_for_date
-    # validates :date_of_hiring, custom_validation_method_for_date
-
-    # Other attributes
-    # validates :pincode, numericality: { only_integer: true, greater_than_or_equal_to: 0 },
-    #  allow_blank: true
-    # validates :city, :state, length: { maximum: 255 }, allow_blank: true
+    def age 
+      return nil unless date_of_birth.present?
+        current_date = Date.today
+        age = current_date.year - date_of_birth.year - ((current_date.month > date_of_birth.month || 
+          (current_date.month == date_of_birth.month && current_date.day >= date_of_birth.day)) ? 0: 1)
+        return age
+    end
 end
